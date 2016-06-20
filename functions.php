dac85c94cbebc3e7f8b48edeca1a3ac51d5798ef
Lisa Wells <lisawells999@gmail.com>
2016-06-20 14:32:11 -0400

    enqueue late; begin to style map-entry

--- functions.php
+++ functions.php
@@ -6,3 +6,11 @@ add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
+
+add_action( 'wp_enqueue_scripts', 'ddraleigh_enqueue_scripts_styles' );
+function ddraleigh_enqueue_scripts_styles() {
+
+    wp_enqueue_style( 'ddraleigh-fonts', '//fonts.googleapis.com/css?family=Lato:300,700,300italic', array(), CHILD_THEME_VERSION );
+
+
+}
